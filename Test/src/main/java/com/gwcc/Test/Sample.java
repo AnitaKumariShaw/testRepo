15214503949b0102ac4ba58f0ff7b5128b5c2acc
anita.kumari.shaw <joe@dev.org>
2019-06-03 13:28:22 +0530

    Pushing a new file to origin

---
+++ Test/src/main/java/com/gwcc/Test/Sample.java
@@ -0,0 +1,10 @@
+package com.gwcc.Test;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Created a new class to push it to git");
+	}
+
+}
