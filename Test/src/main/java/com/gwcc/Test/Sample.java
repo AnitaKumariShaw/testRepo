b514bf4717cc998f722b43fa83c4aca4191643a0
anita.kumari.shaw <anitashaw>
2019-06-03 17:56:29 +0530

    commit the latest change

--- Test/src/main/java/com/gwcc/Test/Sample.java
+++ Test/src/main/java/com/gwcc/Test/Sample.java
@@ -4,7 +4,8 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Created a new class to push it to git");
+		System.out.println("Created a new sample class to push it to git");
 	}
+
 
 }
